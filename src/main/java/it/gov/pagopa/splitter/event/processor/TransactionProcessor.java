014932ddc95ec6f7e2971b361f309c512c17c596
GiovanaSolorzano <gisolorz@emeal.nttdata.com>
2022-09-27 17:36:18 +0200

    IREER-157 Fixed Consumer TransactionProcessor

--- src/main/java/it/gov/pagopa/splitter/event/processor/TransactionProcessor.java
+++ src/main/java/it/gov/pagopa/splitter/event/processor/TransactionProcessor.java
@@ -2,11 +2,8 @@ package it.gov.pagopa.splitter.event.processor;
 
 import it.gov.pagopa.splitter.service.UserIdSplitterMediator;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.binder.Binding;
-import org.springframework.cloud.stream.binder.BindingCreatedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 
@@ -32,12 +29,4 @@ public class TransactionProcessor{
         return userIdSplitterMediator::execute;
     }
 
-    @EventListener(BindingCreatedEvent.class)
-    public void onBindingCreatedEvent(BindingCreatedEvent event) {
-        if (event.getSource() instanceof Binding<?> binding && TRX_PROCESSOR_BINDING_NAME.equals(binding.getBindingName())) {
-            synchronized (this) {
-                binding.start();
-            }
-        }
-    }
 }
