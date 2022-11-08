aaedcbf5c5fba5df0b05b11b70b544c122270f0e
anttorre <antonio.torre@nttdata.com>
2022-11-08 16:19:40 +0100

    fixed BaseIntegrationTest

--- src/test/java/it/gov/pagopa/splitter/test/utils/TestUtils.java
+++ src/test/java/it/gov/pagopa/splitter/test/utils/TestUtils.java
@@ -8,6 +8,7 @@ import it.gov.pagopa.splitter.dto.TransactionEnrichedDTO;
 import it.gov.pagopa.splitter.dto.TransactionRejectedDTO;
 import it.gov.pagopa.splitter.utils.RewardUserIdSplitterConstants;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Header;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.util.ReflectionUtils;
 
@@ -70,7 +71,8 @@ public class TestUtils {
 
     /** To read {@link org.apache.kafka.common.header.Header} value */
     public static String getHeaderValue(ConsumerRecord<String, String> errorMessage, String errorMsgHeaderSrcServer) {
-        return new String(errorMessage.headers().lastHeader(errorMsgHeaderSrcServer).value());
+        Header header = errorMessage.headers().lastHeader(errorMsgHeaderSrcServer);
+        return header!=null? new String(header.value()) : null;
     }
 
 }
